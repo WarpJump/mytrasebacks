839158e7714bc3abda78abf8d9a12d39b0d1f7a8
Danila Gushchin <warpjump@bk.ru>
2023-08-29 14:13:32 +0300

    added colors file

---
+++ colors.h
@@ -0,0 +1,29 @@
+/*!
+\file
+\brief Header file containing visual text macroses
+*/
+
+/*!
+  \brief Macros that paints text in branches in red
+*/
+#define RedText(text) "\033[0;31m" text "\033[0m"
+
+/*!
+  \brief Macros that paints text in branches in orange
+*/
+#define OrangeText(text) "\033[93m" text "\033[0m"
+
+/*!
+  \brief Macros that paints text in branches in magenta
+*/
+#define MagentaText(text) "\033[35m" text "\033[0m"
+
+/*!
+  \brief Macros that paints text in branches in blue
+*/
+#define BlueText(text) "\033[31m" text "\033[0m"
+
+/*!
+  \brief Macros that paints text in branches in green
+*/
+#define GreenText(text) "\033[0;32m" text "\033[0m"
